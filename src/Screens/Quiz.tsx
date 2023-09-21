48be610bb33528cc456bd5efde07d50576749984
David Edwards <david.edwards@hsu-foundation.org>
2023-09-21 08:14:54 -0500

    added to github after Result Page

--- src/Screens/Quiz.tsx
+++ src/Screens/Quiz.tsx
@@ -1,35 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { styles } from '../Styles/Styling'
+import { useNavigation } from '@react-navigation/native';
 
-const Quiz = () => {
-  return (
-    <View style={{backgroundColor:'green', flex:1, padding:10,}}>
-     <View style={styles.quizContainer}>
-         <View style={styles.heading}>
-            <Text style={styles.headingText}>
-                The Alphabet
-            </Text>
-         </View>
-         <View style={{marginTop:10,padding:10,}}>
-         <View style={styles.question}>
-            <Text style={styles.questionText}>Q. Ramiro es mas guapo</Text>
-         </View>
-         <View style={styles.answers}>
-            <Text style={styles.answerText}>(a): que  </Text>
-            <Text style={styles.answerText}>(b): como
- </Text>
-            <Text style={styles.answerText}>
-                (c): de
-            </Text>
-            <Text style={styles.answerText}>
-                (d): que 
-            </Text>
-         </View>
-         </View>
-     </View>
-    </View>
-  )
+const Quiz = (navigation:any) => {
+    navigation= useNavigation();
+    return (
+        <View style={{ backgroundColor: 'green', flex: 1, padding: 10, }}>
+            <View style={styles.quizContainer}>
+                <View style={styles.heading}>
+                    <Text style={styles.headingText}>
+                        The Alphabet
+                    </Text>
+                </View>
+                <View style={{ marginTop: 10, padding: 10, }}>
+                    <View style={styles.question}>
+                        <Text style={styles.questionText}>Q. Ramiro es mas guapo</Text>
+                    </View>
+                    <View style={styles.answers}>
+                        <Text style={styles.answerText}>(a): que  </Text>
+                        <Text style={styles.answerText}>(b): como
+                        </Text>
+                        <Text style={styles.answerText}>
+                            (c): de
+                        </Text>
+                        <Text style={styles.answerText}>
+                            (d): que
+                        </Text>
+                    </View>
+                </View>
+            </View>
+            <View style={{alignItems:'center', justifyContent:'center', marginTop:100}}>
+             <TouchableOpacity onPress={()=>navigation.navigate('Result')}>
+                <Text style={styles.headingText}>Next</Text>
+             </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 export default Quiz
--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../Screens/Home';
 import Quiz from '../Screens/Quiz';
+import Result from '../Screens/Result';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ function MyStack() {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
       <Stack.Screen name="Quiz" component={Quiz} options={{headerShown:false}} />
+      <Stack.Screen name="Result" component={Result} options={{headerShown:false}} />
     
     </Stack.Navigator>
   );
