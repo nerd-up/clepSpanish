906d5c935ddf8b4a5526cdd9a1f06b7cd6981d2b
Laptop User <LaptopUser@JCPL-MVRC-012.argo.uwf.edu>
2023-09-25 01:31:01 -0500

    made the home screen dynamic

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -3,16 +3,28 @@ import React, { useEffect, useState } from 'react'
 import { styles } from '../Styles/Styling'
 import { useNavigation } from '@react-navigation/native'
 import firestore from '@react-native-firebase/firestore'
-import database from '@react-native-firebase/database';
+import database, { firebase } from '@react-native-firebase/database';
 const Home = () => {
   interface clepSpanish{
     sectionNo:string
   }
-  const [section, setSection]=useState();
+  let sections: any=[]
+  const [section, setSection]=useState([]);
   const loadSections=async()=>{
-    const user = await firestore().collection('clepSpanish').doc('SgebzoKaYf6aP3f3t9Ax').get();
-    console.log(user._data.Drill1);
-    setSection(user._data.Drill1);
+    const databaseRef = firebase.database().ref('/sections');
+
+    // Fetch data from the database
+    databaseRef.once('value')
+      .then((snapshot) => {
+        const data = snapshot.val();
+        for (const i in data){
+          sections.push(i);
+        }
+        setSection(sections);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }
   
   useEffect(()=>{
@@ -23,17 +35,13 @@ const Home = () => {
     <View style={styles.homePageContainer} >
     <View style={styles.headingContainer}><Text style={styles.headingStyle}>Spanish Exam</Text></View>
       <Text style={styles.selectSectionStyle}>Select Section</Text>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 1'})} ><Text style={styles.sectionStyle}>{section}</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 2'})} ><Text style={styles.sectionStyle}>Dos</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 3'})} ><Text style={styles.sectionStyle}>Tres</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 4'})} ><Text style={styles.sectionStyle}>Quatro</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 5'})} ><Text style={styles.sectionStyle}>Cinco</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 6'})} ><Text style={styles.sectionStyle}>Seis</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 7'})} ><Text style={styles.sectionStyle}>Siete</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 8'})} ><Text style={styles.sectionStyle}>Ocho</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 9'})} ><Text style={styles.sectionStyle}>Nueve</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 10'})} ><Text style={styles.sectionStyle}>Deiz</Text></TouchableOpacity>
-    </View>
+       {
+        section.map((data:any)=>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 1'})} >
+          <Text style={styles.sectionStyle}>{data}</Text>
+        </TouchableOpacity>
+      )}
+     </View>
   )
 }
 
