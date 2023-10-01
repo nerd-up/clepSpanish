a1feeda3187f98a5cf3492b0fdea08c7f9b20e16
Laptop User <LaptopUser@JCPL-MVRC-012.argo.uwf.edu>
2023-10-01 09:02:53 -0500

    question indexes

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -10,37 +10,44 @@ const Home = () => {
   }
   let sections: any=[]
   const [section, setSection]=useState([]);
-  const loadSections=async()=>{
-    const databaseRef = firebase.database().ref('/sections');
-
-    // Fetch data from the database
-    databaseRef.once('value')
-      .then((snapshot) => {
-        const data = snapshot.val();
-        for (const i in data){
-          sections.push(i);
-        }
-        setSection(sections);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }
   
-  useEffect(()=>{
-    loadSections();
-  },[])
-    const navigation= useNavigation();
+  
+  const navigation= useNavigation();
   return (
     <View style={styles.homePageContainer} >
     <View style={styles.headingContainer}><Text style={styles.headingStyle}>Spanish Exam</Text></View>
       <Text style={styles.selectSectionStyle}>Select Section</Text>
-       {
-        section.map((data:any)=>
-        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 1'})} >
-          <Text style={styles.sectionStyle}>{data}</Text>
+      <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 1'})} >
+          <Text style={styles.sectionStyle}>Uno</Text>
         </TouchableOpacity>
-      )}
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 2'})} >
+          <Text style={styles.sectionStyle}>Dos</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 3'})} >
+          <Text style={styles.sectionStyle}>Tres</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 4'})} >
+          <Text style={styles.sectionStyle}>Coatro</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 5'})} >
+          <Text style={styles.sectionStyle}>Tinco</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 6'})} >
+          <Text style={styles.sectionStyle}>Seis</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 7'})} >
+          <Text style={styles.sectionStyle}>Siete</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 8'})} >
+          <Text style={styles.sectionStyle}>Ocho</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 9'})} >
+          <Text style={styles.sectionStyle}>Nuede</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.moduleBtn} onPress={()=>navigation.navigate('Quiz',{section:'Drill 10'})} >
+          <Text style={styles.sectionStyle}>Deiz</Text>
+        </TouchableOpacity>
+
      </View>
   )
 }
--- src/Screens/Quiz.tsx
+++ src/Screens/Quiz.tsx
@@ -1,53 +1,78 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity,ActivityIndicator } from 'react-native'
 import React,{useState,useEffect} from 'react'
 import { styles } from '../Styles/Styling'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import firestore from '@react-native-firebase/firestore'
-import database from '@react-native-firebase/database';
+import database, { firebase } from '@react-native-firebase/database';
+
 
 const Quiz = (navigation:any) => {
-    const loadData=()=>{
-        database()
-        .ref('/sections/Dos/')
-        .once('value')
-        .then(snapshot => {
-          console.log('User data: ', snapshot.val());
+    const [dbQuestions,setDbQuestions]=useState([]);
+    const [score, setScores]=useState(0);
+    const [currentQuestionIndex, setCurrentQuestionIndex]=useState(0);
+    const [options, setOptions]=useState([]);
+    const setQuestionOptions=(index:any)=>{
+        let setOption=[]
+        if(index>=0){
+            let setOption=dbQuestions[index]?.wrongAnswer;
+            setOption.push(dbQuestions[index]?.correctAnswer);
+            // console.log(setOption);
+            setOptions(setOption);
+            console.log(options.length);
+        }
+    }
+    const setNextQuestion=()=>{
+        setCurrentQuestionIndex(currentQuestionIndex+1);
+        setQuestionOptions(currentQuestionIndex);
+    }
+        const loadData=()=>{
+        const database=firebase.database();
+        const loadSection=database.ref('sections/Uno');
+        loadSection.on('value',(snapshot)=>{
+           const data=snapshot.val();
+           if(data!=null){
+            const questions:any=Object.values(data);
+            // console.log(questions);
+            setDbQuestions(questions);
+            setQuestionOptions(currentQuestionIndex);
+            // console.log(dbQuestions);
+           }
         });
-      }
-      useEffect(()=>{loadData()},[])
+    }
+    useEffect(()=>{loadData()},[])
     const route=useRoute();
     const sectionNumber=route.params?.section
-    const [selected, setSelected] = useState(false);
-    const updateSelected=()=>{
-        setSelected(true);
+    const [selected, setSelected] = useState(-1);
+    const updateSelected=(selectedIndex:any)=>{
+        setSelected(selectedIndex);
     }
     navigation= useNavigation();
     return (
         <View style={{ backgroundColor: 'green', flex: 1, padding: 10, }}>
-            <View style={styles.quizContainer}>
-                <View style={styles.heading}>
-                    <Text style={styles.headingText}>
-                        {route.params?.section}
-                    </Text>
-                </View>
-                <View style={{ marginTop: 10, padding: 10, }}>
-                    <View style={styles.question}>
-                        <Text style={styles.questionText}>Q. Ramiro es mas guapo</Text>
-                    </View>
-                    <View style={styles.answers}>
-                        <TouchableOpacity onPress={updateSelected}><Text style={selected?styles.selectedAnswerText:styles.answerText}>(a): que  </Text></TouchableOpacity>
-                        <TouchableOpacity onPress={updateSelected}><Text style={selected?styles.selectedAnswerText:styles.answerText}>(b): como</Text></TouchableOpacity>
-                        <TouchableOpacity onPress={updateSelected}><Text style={selected?styles.selectedAnswerText:styles.answerText}>(c): de</Text></TouchableOpacity>
-                        <TouchableOpacity onPress={updateSelected}><Text style={selected?styles.selectedAnswerText:styles.answerText}>(d): que</Text></TouchableOpacity>
-                    </View>
-                </View>
+        <View style={styles.quizContainer}>
+            <View style={styles.heading}>
+                <Text style={styles.headingText}>
+                    {route.params?.section}
+                </Text>
             </View>
-            <View style={{alignItems:'center', justifyContent:'center', marginTop:100}}>
-             <TouchableOpacity onPress={()=>navigation.navigate('Result')}>
-                <Text style={styles.headingText}>Next</Text>
-             </TouchableOpacity>
+            <View style={{ marginTop: 10, padding: 10, }}>
+                <View style={styles.question}>
+                    <Text style={styles.questionText}>Q. {dbQuestions[currentQuestionIndex]?.question}</Text> 
+                </View>
+                <View style={styles.answers}>
+                    <TouchableOpacity onPress={()=>updateSelected(0)}><Text style={selected==0?styles.selectedAnswerText:styles.answerText}>(a): {options[0]}</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={()=>updateSelected(1)}><Text style={selected==1?styles.selectedAnswerText:styles.answerText}>(b): {options[1]}</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={()=>updateSelected(2)}><Text style={selected==2?styles.selectedAnswerText:styles.answerText}>(c): {options[2]}</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={()=>updateSelected(3)}><Text style={selected==3?styles.selectedAnswerText:styles.answerText}>(d): {options[3]}</Text></TouchableOpacity>
+                </View>
             </View>
         </View>
+        <View style={{alignItems:'center', justifyContent:'center', marginTop:100}}>
+         <TouchableOpacity onPress={dbQuestions.length>currentQuestionIndex?()=>setNextQuestion():()=>navigation.navigate('Result')}>
+            <Text style={styles.headingText}>Next</Text>
+         </TouchableOpacity>
+        </View>
+    </View>
     )
 }
 
